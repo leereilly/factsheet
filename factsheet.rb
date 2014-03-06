d8bf7f895f1c93520a064164dc2501942f404104
Lee Reilly <lee@leereilly.net>
2014-03-05 21:21:57 -0800

    Gather the Alexa rank from AWS

--- factsheet.rb
+++ factsheet.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'sinatra'
 require 'pdfkit'
 require 'numbers_and_words'
+require 'alexa'
 
 PDFKit.configure do |config|
   config.wkhtmltopdf = '/opt/boxen/rbenv/shims/wkhtmltopdf'
@@ -13,9 +14,19 @@ PDFKit.configure do |config|
   }
 end
 
+def get_alexa_rank
+  access_key_id     = ENV['AWS_ACCESS_KEY_ID']
+  secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+
+  client = Alexa::Client.new(access_key_id: access_key_id, secret_access_key: secret_access_key)
+  url_info = client.url_info(url: "github.com")
+  url_info.rank
+end
+
 get '/' do
   @total_users = 5_400_000
   @total_repos = 11_300_000
+  @alexa_rank  = get_alexa_rank
 
   headers({ 'Content-Type' => 'application/pdf',
     'Content-Description' => 'File Transfer',
